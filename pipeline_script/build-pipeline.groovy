96213156beefef7c41347389d64482de104e8e71
raima <raimamandal200@gmail.com>
2025-03-07 21:17:53 +0530

    Add path /usr/local/bin/

--- pipeline_script/build-pipeline.groovy
+++ pipeline_script/build-pipeline.groovy
@@ -94,10 +94,10 @@ pipeline
                         export PATH=$PATH:/usr/local/bin 
                         echo "DEBUG: AWS_ACCESS_KEY is set to: $AWS_ACCESS_KEY"
                         echo "DEBUG: AWS_SECRET_KEY is set to: $AWS_SECRET_KEY" 
-                        aws configure set aws_access_key_id $AWS_ACCESS_KEY
-                        aws configure set aws_secret_access_key $AWS_SECRET_KEY
-                        aws ecr describe-repositories --repository-names ${params.ECR_REPO_NAME} --region ap-south-1 || \
-                        aws ecr create-repository --repository-name ${params.ECR_REPO_NAME} --region ap-south-1
+                        /usr/local/bin/aws configure set aws_access_key_id $AWS_ACCESS_KEY
+                        /usr/local/bin/aws configure set aws_secret_access_key $AWS_SECRET_KEY
+                        /usr/local/bin/aws ecr describe-repositories --repository-names ${params.ECR_REPO_NAME} --region ap-south-1 || \
+                        /usr/local/bin/aws ecr create-repository --repository-name ${params.ECR_REPO_NAME} --region ap-south-1
                     """
                 }
             }
